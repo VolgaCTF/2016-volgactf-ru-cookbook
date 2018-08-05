f208272496d1eea723589d0f0fd9e1e99b104861
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2018-08-05 20:11:03 +0400

    Update version

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name '2016-volgactf-ru'
 description 'Installs and configures 2016.volgactf.ru'
-version '1.2.0'
+version '1.2.1'
 
 recipe 'volgactf-ru', 'Installs and configures 2016.volgactf.ru'
 depends 'nodejs'
 depends 'nginx'
-depends 'tls', '~> 3.0.0'
+depends 'tls', '~> 3.0.3'
 depends 'instance', '~> 2.0.0'
